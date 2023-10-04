3641396b35015688e362e3cc528d12e8f17c3f3f
Sergey1one <sm.bydin@gmail.com>
2023-10-04 12:13:08 +0300

    Redux-persist phonebook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,7 @@
 
 import { GlobalStyle } from "./Global Styles";
-import { nanoid } from 'nanoid';
-import { ToastContainer, toast } from 'react-toastify';
+
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Section from "./Section";
 import Contacts from "./Contacts";
@@ -14,56 +14,7 @@ import { FormContact } from "./Form/Form";
 
 
 export function App(){
-  // const [contacts, setContacts] = useState(()=>{return JSON.parse(window.localStorage.getItem('contacts')) ?? []}   )
-  // const [filter,setFilter]=useState('')
-
-  // useEffect(() => { 
-  //   window.localStorage.setItem('contacts', JSON.stringify(contacts))
-  // }, [contacts])
   
-
-  // const formSubmitHandler = (  name, number ) => {
-    
-    
-  //   if (contacts.find(
-  //     contact => contact.name.toLowerCase() === name.toLowerCase())) {
-  //     toast(`Contact with name ${name} -already in contact list`)
-  //   }
-  //   else if(contacts.find(
-  //     contact => contact.number.toLowerCase() === number.toLowerCase())) {
-  //     toast(`Contact with this ${number} number  -already in contact list`)
-  //   }
-  //   else if (name.trim() === '' || number.trim() === '') {
-  //     toast(`Enter name or number phone correct !!`)
-  //   }
-  //   else if (!/\d{3}[-]\d{2}[-]\d{2}/g.test(number)) {
-  //       toast.error('💩 Enter the correct number phone!');
-  //   }
-  //   // else if (!/^[a-zA-Zа-яёА-ЯЁ\s\-]+$/.test(name)) {
-  //   //    toast.error('💩 Enter the correct name!');
-  //   //   }
-  //   else {
-  //     const contact = { id:nanoid(), name, number };
-  //   setContacts(prevContacts => [contact, ...prevContacts])
-  //   }
-  // }
-
-  // const changeFilter=(e)=>{setFilter(e.currentTarget.value)}
-
-  // const getVisibleContacts = () => {
-  //   const normalizedFilter = filter.toLowerCase();
-   
-  //    return contacts.filter(contact=>contact.name.toLowerCase().includes(normalizedFilter))
-  // }
-
-  // const deleteContact = idContact => {
-  //  setContacts(contacts.filter(({id})=>id!==idContact))
-  //   }
-  
-
-
-
- 
   return (
    
     <Box width='400px' pl='16px'>
